e2515316c45ef6897a79936a5c318622c3fbea69
Cristóbal Segovia R. <cr.segoviar@duocuc.cl>
2026-06-15 23:21:31 -0400

    se cambia entorno de ejecucion a local

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -186,4 +186,4 @@ def actualizar_dashboard(categoria_seleccionada):
 # 5. Ejecución del Dashboard localmente
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8050)
+    app.run(debug=True, port=8050)
